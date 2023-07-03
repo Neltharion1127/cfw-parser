68a576d6df41d147ee819e4e0477811ca6d7d051
Neltharion1127 <454537184@qq.com>
2023-07-03 20:13:44 +0800

    feat: some rules

--- dist/cfw-parser.js
+++ dist/cfw-parser.js
@@ -368,12 +368,16 @@ function requireRules() {
   hasRequiredRules = 1;
   rules = {
     // 普通代理
-    useProxyRule: ["DOMAIN-KEYWORD,github", "DOMAIN-KEYWORD,spotify", "DOMAIN-SUFFIX,wikipedia.org", "DOMAIN-KEYWORD,epic", "DOMAIN-KEYWORD,afreecatv.com", "DOMAIN-KEYWORD,spotify", "DOMAIN-SUFFIX,op.gg", "DOMAIN-SUFFIX,thepiratebay.org", "DOMAIN-SUFFIX,reactjs.org", "DOMAIN-SUFFIX,akamaized.net", "DOMAIN-SUFFIX,wikimedia.org", "DOMAIN-SUFFIX,huo720.com", "DOMAIN-SUFFIX,v2ex.com", "DOMAIN-SUFFIX,subhd.la", "DOMAIN-SUFFIX,amazonaws.com", "DOMAIN-SUFFIX,discord.com", "DOMAIN-SUFFIX,duyaoss.com", "DOMAIN-SUFFIX,gravatar.com"],
+    useProxyRule: ["DOMAIN-KEYWORD,github", "DOMAIN-KEYWORD,spotify", "DOMAIN-SUFFIX,wikipedia.org", "DOMAIN-KEYWORD,epic", "DOMAIN-KEYWORD,afreecatv.com", "DOMAIN-KEYWORD,spotify", "DOMAIN-SUFFIX,op.gg", "DOMAIN-SUFFIX,thepiratebay.org", "DOMAIN-SUFFIX,reactjs.org", "DOMAIN-SUFFIX,akamaized.net", "DOMAIN-SUFFIX,wikimedia.org", "DOMAIN-SUFFIX,huo720.com", "DOMAIN-SUFFIX,v2ex.com", "DOMAIN-SUFFIX,subhd.la", "DOMAIN-SUFFIX,amazonaws.com", "DOMAIN-SUFFIX,discord.com", "DOMAIN-SUFFIX,duyaoss.com", "DOMAIN-SUFFIX,gravatar.com", "DOMAIN-SUFFIX,readwise.io", "DOMAIN-SUFFIX,redd.it", "DOMAIN-SUFFIX,redditstatic.com", "DOMAIN-SUFFIX,fengzg.net", "DOMAIN-SUFFIX,stackoverflow.com", "DOMAIN-SUFFIX,nextjs.org", "DOMAIN-SUFFIX,ubuntu.com", "DOMAIN-SUFFIX,windows.net", "DOMAIN-SUFFIX,csgostats.gg", "DOMAIN-SUFFIX,netflav.com", "DOMAIN-SUFFIX,riotgames.com", "DOMAIN-KEYWORD,steamcommunity.com", "DOMAIN-SUFFIX,paypal.com", "DOMAIN-KEYWORD,openwrt.org"],
     // 特定域名的代理
-    specialRules: ["DOMAIN-SUFFIX,openai.com,OpenAI"],
+    specialRules: ["DOMAIN-SUFFIX,openai.com,OpenAI", "IP-CIDR,140.83.85.183/32,Oracle VPS", "IP-CIDR,152.69.193.156/32,Oracle VPS"],
     // 代理组的名称
     specialGroup: [{
       name: "OpenAI",
+      type: "select",
+      proxies: null
+    }, {
+      name: "Oracle VPS",
       type: "select",
       proxies: null
     }]
@@ -392,7 +396,7 @@ function requireCommonFunction() {
       useProxyRule = _require$$.useProxyRule,
       specialRules = _require$$.specialRules,
       specialGroup = _require$$.specialGroup;
-    // useProxyRule不能直接修改原数组，clash没有关闭的情况下会反复引用修改后的数组
+    // 不能直接修改原数组，clash没有关闭的情况下会反复引用修改后的数组
     // 添加rule 默认将useProxyRule中的规则添加第一条规则
     (_obj$rules = obj["rules"]).unshift.apply(_obj$rules, _toConsumableArray(useProxyRule.map(function (item) {
       return item + "," + proxyGroupName;
--- src/rules.js
+++ src/rules.js
@@ -1,34 +1,56 @@
 module.exports = {
-  // 普通代理
-  useProxyRule : [
-    "DOMAIN-KEYWORD,github",
-    "DOMAIN-KEYWORD,spotify",
-    "DOMAIN-SUFFIX,wikipedia.org",
-    "DOMAIN-KEYWORD,epic",
-    "DOMAIN-KEYWORD,afreecatv.com",
-    "DOMAIN-KEYWORD,spotify",
-    "DOMAIN-SUFFIX,op.gg",
-    "DOMAIN-SUFFIX,thepiratebay.org",
-    "DOMAIN-SUFFIX,reactjs.org",
-    "DOMAIN-SUFFIX,akamaized.net",
-    "DOMAIN-SUFFIX,wikimedia.org",
-    "DOMAIN-SUFFIX,huo720.com",
-    "DOMAIN-SUFFIX,v2ex.com",
-    "DOMAIN-SUFFIX,subhd.la",
-    "DOMAIN-SUFFIX,amazonaws.com",
-    "DOMAIN-SUFFIX,discord.com",
-    "DOMAIN-SUFFIX,duyaoss.com",
-    "DOMAIN-SUFFIX,gravatar.com",
-  ],
-  // 特定域名的代理
-  specialRules :["DOMAIN-SUFFIX,openai.com,OpenAI"],
-  // 代理组的名称
-  specialGroup: [
-    {
-      name: "OpenAI",
-      type: "select",
-      proxies: null,
-    },
-  ]
-  
-}
+    // 普通代理
+    useProxyRule: [
+        "DOMAIN-KEYWORD,github",
+        "DOMAIN-KEYWORD,spotify",
+        "DOMAIN-SUFFIX,wikipedia.org",
+        "DOMAIN-KEYWORD,epic",
+        "DOMAIN-KEYWORD,afreecatv.com",
+        "DOMAIN-KEYWORD,spotify",
+        "DOMAIN-SUFFIX,op.gg",
+        "DOMAIN-SUFFIX,thepiratebay.org",
+        "DOMAIN-SUFFIX,reactjs.org",
+        "DOMAIN-SUFFIX,akamaized.net",
+        "DOMAIN-SUFFIX,wikimedia.org",
+        "DOMAIN-SUFFIX,huo720.com",
+        "DOMAIN-SUFFIX,v2ex.com",
+        "DOMAIN-SUFFIX,subhd.la",
+        "DOMAIN-SUFFIX,amazonaws.com",
+        "DOMAIN-SUFFIX,discord.com",
+        "DOMAIN-SUFFIX,duyaoss.com",
+        "DOMAIN-SUFFIX,gravatar.com",
+        "DOMAIN-SUFFIX,readwise.io",
+        "DOMAIN-SUFFIX,redd.it",
+        "DOMAIN-SUFFIX,redditstatic.com",
+        "DOMAIN-SUFFIX,fengzg.net",
+        "DOMAIN-SUFFIX,stackoverflow.com",
+        "DOMAIN-SUFFIX,nextjs.org",
+        "DOMAIN-SUFFIX,ubuntu.com",
+        "DOMAIN-SUFFIX,windows.net",
+        "DOMAIN-SUFFIX,csgostats.gg",
+        "DOMAIN-SUFFIX,netflav.com",
+        "DOMAIN-SUFFIX,riotgames.com",
+        "DOMAIN-KEYWORD,steamcommunity.com",
+        "DOMAIN-SUFFIX,paypal.com",
+        "DOMAIN-KEYWORD,openwrt.org"
+    ],
+    // 特定域名的代理
+    specialRules: [
+        "DOMAIN-SUFFIX,openai.com,OpenAI",
+        "IP-CIDR,140.83.85.183/32,Oracle VPS",
+        "IP-CIDR,152.69.193.156/32,Oracle VPS"
+    ],
+    // 代理组的名称
+    specialGroup: [
+        {
+            name: "OpenAI",
+            type: "select",
+            proxies: null,
+        },
+        {
+            name: "Oracle VPS",
+            type: "select",
+            proxies: null,
+        },
+    ],
+};
